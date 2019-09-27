feff28b05a84b19ed3653ce5d71752f1c36d3c99
Justin Chin <justinmichaelchin@gmail.com>
2019-09-26 18:42:24 -0700

    formatting changes

--- arith.rb
+++ arith.rb
@@ -1,9 +1,18 @@
+#!/usr/bin/ruby
+
+# arith.rb
+
+# connects to a remote tuplespace, and takes any match where the first
+# element is in the set {+-*/} and the second and third elements are
+# numbers, writing the results of the first element applied as an
+# operation to the second and third
+
 require 'rinda/rinda'
 
 URI = "druby://localhost:61676"
 DRb.start_service
 ts = Rinda::TupleSpaceProxy.new(DRbObject.new(nil, URI))
 loop do
-  ops, a, b = ts.take([ %r{^[-+/*]$}, Numeric, Numeric])
+  ops, a, b = ts.take([%r{^[-+/*]$}, Numeric, Numeric])
   ts.write(["result", a.send(ops, b)])
 end
