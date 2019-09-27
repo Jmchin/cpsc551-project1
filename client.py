186b8133903e496863308b102784ec351f7069fc
Justin Chin <justinmichaelchin@gmail.com>
2019-09-26 18:42:57 -0700

    allows NoneType vals to be passed, writes polish notation tuple out

--- client.py
+++ client.py
@@ -7,12 +7,13 @@
 
 from xmlrpc.client import ServerProxy, Error
 
-with ServerProxy("http://localhost:8000") as proxy:
+with ServerProxy("http://localhost:8000", allow_none=True) as proxy:
     try:
-        tuples = (("*", 2, 2), ("+", 2, 5), ("-", 9, 3))
+        tuples = [("*", 2, 2), ("+", 2, 5), ("-", 9, 3)]
         for t in tuples:
-            proxy.microblog._out(t)
-            res = proxy.microblog._in("result", None)
+            ops, a, b = t
+            proxy.arith._out(ops, a, b)
+            res = proxy.arith._in("result", None, None)
             print(f'{res[1]} = {t[1]} {t[0]} {t[2]}')
     except Error as e:
         print(f'Error: {e}')
